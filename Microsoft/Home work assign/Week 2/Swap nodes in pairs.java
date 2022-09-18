2d0fb847c9c415b0e383a00488751db9f06cef32
GowshikanS <113593526+GowshikanS@users.noreply.github.com>
2022-09-18 19:57:30 +0530

    Create Swap nodes in pairs.java

---
+++ Microsoft/Home work assign/Week 2/Swap nodes in pairs.java
@@ -0,0 +1,21 @@
+public ListNode swapPairs(ListNode head) {
+        if (head==null || head.next==null)return head; 
+        
+       
+        ListNode temp=head.next;
+        head.next=head.next.next; 
+        temp.next=head; 
+        head=temp;
+
+       
+        ListNode curr=head.next; 
+        while(curr.next!=null && curr.next.next!=null){
+            temp=curr.next;
+            curr.next=curr.next.next;
+            temp.next=temp.next.next;
+            curr.next.next=temp;
+            curr=curr.next.next;
+        }
+        
+        return head;
+    }
