9644e6dab5cefc07911de6a78354905783d177f6
GowshikanS <113593526+GowshikanS@users.noreply.github.com>
2022-09-18 21:03:59 +0530

    Create roundoff.java

---
+++ Google Assignments/roundoff.java
@@ -0,0 +1,8 @@
+public class main{
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double num =sc.nextDouble();
+        System.out.format("%.2f", num);
+    }
+}
