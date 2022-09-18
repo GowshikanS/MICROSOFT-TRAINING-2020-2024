58b0211b7e62fb8155a8659d59710e190e5c80dc
GowshikanS <113593526+GowshikanS@users.noreply.github.com>
2022-09-18 20:52:08 +0530

    Create sum.java

---
+++ Google Assignments/sum.java
@@ -0,0 +1,17 @@
+public class Sum 
+{  
+public static void main(String[] args)   
+{  
+Scanner sc=new Scanner(System.in);
+int num=sc.nextInt();
+int i=0;
+
+for(i = 1; i <= num; ++i)  
+{  
+
+sum = sum + i;  
+}  
+
+System.out.println(sum);  
+}  
+}  
