2e60cb81bd34d2acdae922cc26668825fed6959d
GowshikanS <113593526+GowshikanS@users.noreply.github.com>
2022-09-18 20:49:46 +0530

    Create Area.java

---
+++ Google Assignments/Area.java
@@ -0,0 +1,10 @@
+import java. util. Scanner; 
+class Main{
+public static void main(String args[]){
+Scanner s= new Scanner(System. in);
+System. out. println("Enter the radius:");
+double r= s. nextDouble();
+double area=(22*r*r)/7 ;
+System. out. println(area); 
+} 
+}
