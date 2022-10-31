880b42a703ccc3401f62604f01fec1dbfd7de0d4
GowshikanS <113593526+GowshikanS@users.noreply.github.com>
2022-10-31 20:57:04 +0530

    Create Design HashSet.java

---
+++ Microsoft/Home work assign/Week 7/Design HashSet.java
@@ -0,0 +1,32 @@
+class MyHashSet {
+    int num[];
+    public MyHashSet() {
+        num = new int[31251];
+    }
+	
+	
+    public void add(int key) {
+        num[getIdx(key)]|=getMask(key);
+    }
+
+    public void remove(int key) {
+        num[getIdx(key)] &= (~getMask(key));
+    }
+	
+	
+    public boolean contains(int key) {
+        return (num[getIdx(key)]&getMask(key))!=0;
+    }
+	
+	
+    private int getIdx(int key)
+    {
+        return (key/32);
+    }
+
+    private int getMask(int key)
+    {
+        key%=32;
+        return (1<<key);
+    }
+}
